546e2303779c7bc15b2ebbd405c75c37deb6760c
NieJiHui <44926653+NieJiHui@users.noreply.github.com>
2023-06-19 10:38:18 +0800

    Initial commit

--- springmvc-v1-servlet/web/WEB-INF/form.jsp
+++ springmvc-v1-servlet/web/WEB-INF/form.jsp
@@ -7,10 +7,15 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<form action="/hello" method="post">
+    <input type="text" name="method">
+    <input type="submit" value="提交">
+</form>
+
+</body>
 </html>
--- springmvc-v3-annotation/src/main/java/com/nie/controller/HelloController.java
+++ springmvc-v3-annotation/src/main/java/com/nie/controller/HelloController.java
@@ -1,5 +1,44 @@
-package com.nie.controller;/**
-  * @author Akihi
-  * @date 2023/6/18 4:50 PM
-  */public class HelloController {
+package com.nie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Akihi
+ * @date 2023/6/18 4:50 PM
+ */
+@Controller
+@RequestMapping("/h1")
+public class HelloController {
+    @RequestMapping
+    public String mainPage(Model model){
+        //向模型中添加属性msg与值，可以在JSP页面中取出并渲染
+        model.addAttribute("msg","mainPage");
+        //web-inf/jsp/hello.jsp
+        return "hello";
+    }
+    //真实访问地址 : 项目名/HelloController/hello
+    @RequestMapping("/hello1")
+    public String sayHello(Model model){
+        //向模型中添加属性msg与值，可以在JSP页面中取出并渲染
+        model.addAttribute("msg","hello,SpringMVC1");
+        //web-inf/jsp/hello.jsp
+        return "hello";
+    }
+
+    @RequestMapping("/hello2")
+    public String sayHello2(Model model){
+        //向模型中添加属性msg与值，可以在JSP页面中取出并渲染
+        model.addAttribute("msg","hello,SpringMVC2");
+        //web-inf/jsp/hello.jsp
+        return "hello";
+    }
+    @RequestMapping("/hello3")
+    public String sayHello3(Model model){
+        //向模型中添加属性msg与值，可以在JSP页面中取出并渲染
+        model.addAttribute("msg","hello,SpringMVC3");
+        //web-inf/jsp/hello.jsp
+        return "hello";
+    }
 }
